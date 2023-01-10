bffaf30d5db53af529a70db2b56dd55059a49bbd
42corozco <42corozco@gmail.com>
2023-01-10 13:33:31 +0100

    Error.h `Actualizando mensajes de error`

--- includes/error.h
+++ includes/error.h
@@ -1,9 +1,9 @@
 #ifndef ERROR_H
 # define ERROR__H
 
- const char *usageError = "ping: usage error: Destination address required\n";
+ const char *usageError = "ft_ping: usage error: Destination address required\n";
 
- const char *notKnownError[2] = {"ping: ", ": Name or service not known"};
+ const char *notKnownError[2] = {"ft_ping: ", ": Name or service not known"};
 
 
 #endif
